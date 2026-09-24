839584f5917ca1692ee13dad2b9baf219e235215
tachiplot <tachiki@p-lot.com>
2026-09-24 10:49:23 +0900

    Update commission_pay_simulator-v2.py

--- commission_pay_simulator-v2.py
+++ commission_pay_simulator-v2.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 
 st.set_page_config(
-    page_title="歩合給制度 シミュレーター v3",
+    page_title="歩合給制度 シミュレーター v2",
     page_icon="📊",
     layout="wide"
 )
